e8dfd05d3c53cb2cf2891b03e3a9eb853b1305b0
hiverkiya <hiverkiya@gmail.com>
2021-02-12 09:05:21 -0800

    TEST

---
+++ SPOJ/1.cpp
@@ -0,0 +1,14 @@
+//TEST - Life, the Universe, and Everything
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    while(a!=42)
+    {
+        cout<<a<<endl;
+        cin>>a;
+    }
+    return 0;
+}
