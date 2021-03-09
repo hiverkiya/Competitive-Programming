2b4177e50381e28516a1287acfcffc879c92e683
Himanshu Verkiya <hiverkiya@gmail.com>
2021-03-09 19:18:26 +0530

    Codeforces

---
+++ Codeforces/problems/aPlusB.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define push_back pb
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	long sum=a+b;
+	cout<<sum;
+cin.tie(NULL);
+ios_base::sync_with_stdio(false);
+return 0;
+}
+
---
+++ Codeforces/problems/petyaAndStrings.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define push_back pb
+using namespace std;
+int main()
+{
+	string s,ss;
+	cin>>s>>s;
+	
+cin.tie(NULL);
+ios_base::sync_with_stdio(false);
+return 0;
+}
+
