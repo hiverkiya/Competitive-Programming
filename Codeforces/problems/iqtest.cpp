35f485c7a4158cf80a5c8eb85f69b600d2f724b3
Himanshu Verkiya <hiverkiya@gmail.com>
2021-03-12 17:32:13 +0530

    adding snippe

---
+++ Codeforces/problems/iqtest.cpp
@@ -0,0 +1,37 @@
+/* Default - Himanshu Verkiya */
+// INCLUDING MACROS
+#include<algorithm>
+#include<unordered_map>
+#include<vector>
+#include<unordered_set>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<map>
+#include<chrono>
+#include<iostream>
+using namespace std;
+// DEFINING HEADERS
+#define pb push_back
+#define F(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define MAX INT_MAX
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+#define MOD 1000000007
+#define ppb pop_back;
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+int main()
+{ 
+fastio();
+
+return 0;
+}
